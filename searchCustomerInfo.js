5979721b3ae4f317fdb3075d5ef4c9cf1da0f605
WebDesign-MultiMedia <110946138+WebDesign-MultiMedia@users.noreply.github.com>
2025-08-08 19:06:10 -0400

    Update searchCustomerInfo.js

--- searchCustomerInfo.js
+++ searchCustomerInfo.js
@@ -20,6 +20,70 @@ async function searchByPhone(phonePattern) {
   }
 }
 
+// Create the table
+  const table = document.createElement("table");
+  table.style.background = "gray";
+  table.style.color = "white";
+
+  table.className = "border-collapse border w-full text-center rounded-xl";
+
+  // Table head (only once)
+  table.innerHTML = `
+    <thead class="text-blue-200" id="t">
+      <tr class="bg-green-200">
+        <th class="bg-pink-200">Id</th>
+        <th>Date</th>
+        <th>Time</th>
+        <th>Name</th>
+        <th>Phone</th>
+        <th>Address</th>
+        <th>Items</th>
+        <th>Cost</th>
+        <th>Payment Type</th>
+        <th>P/D</th>
+        </tr>
+    </thead>
+    <tbody></tbody>
+  `;
+
+  const tbody = table.querySelector("tbody");
+
+  // Loop through results to create rows
+  results.forEach((order) => {
+    const row = document.createElement("tr");
+    row.style.background = "#00499c";
+    row.style.color = "white";
+    row.style.textAlign = "center";
+  row.className = ' text-center border-2 border-white'
+
+    row.innerHTML = `
+      <td>${order.Id}</td>
+      <td >${order.Date}</td>
+      <td>${order.Time}</td>
+      <td>${order.Name}</td>
+      <td>${order.Contact}</td>
+      <td>${order.Address}</td>
+      <td>${order.Items}</span></td>
+      <td>$${order.Cost}</td>
+      <td>${order.Payment}</td>
+      <td>${order.PickupDelivery}</td>
+    `;
+
+    row.querySelectorAll('td').forEach(td =>{
+      td.style.padding = "10px";
+    })
+    row.querySelectorAll('td')[0].style.background = 'lightcoral';
+    row.querySelectorAll('td')[0].style.fontWeight = 'bold';
+
+    row.querySelectorAll('td')[7].style.background = 'lightblue';
+    row.querySelectorAll('td')[7].style.width = '10%';
+    row.querySelectorAll('td')[8].style.background = 'lightgreen';
+    row.querySelectorAll('td')[9].style.background = 'lightcoral';
+
+
+    tbody.appendChild(row);
+  });
+
 function renderOrdersTable(results, displayDiv) {
   // scrollable wrapper for mobile
   const wrapper = document.createElement('div');
